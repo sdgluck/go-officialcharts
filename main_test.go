b9c8d1c0902db744d4f82c54bd1e61f9a96a75a9
Sam Gluck <sdgluck@gmail.com>
2020-01-05 19:40:03 +0000

    tidy up tests

--- main_test.go
+++ main_test.go
@@ -6,17 +6,35 @@ import (
 )
 
 func TestGetCharts(t *testing.T) {
+	type args struct {
+		year int
+		month int
+		day int
+	}
+
 	tests := []struct {
 		name string
+		args args
+		expectLength int
+		expectFirstSongTitle string
+		expectLastSongTitle string
 	}{
 		{
 			name: "happy: gets chart for Halloween 1992",
+			args: args{
+				year: 1992,
+				month: 10,
+				day: 31,
+			},
+			expectLength: 75,
+			expectFirstSongTitle: "END OF THE ROAD",
+			expectLastSongTitle: "ROADHOUSE MEDLEY (ANNIVERSARY WALTZ PART 25)",
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			result, err := GetCharts(31, 10, 1992)
+			result, err := GetCharts(tt.args.day, tt.args.month, tt.args.year)
 			if err != nil {
 				t.Fatal(err)
 				return
@@ -24,9 +42,9 @@ func TestGetCharts(t *testing.T) {
 
 			a := assert.New(t)
 
-			a.Len(result.Songs, 75)
-			a.Equal("END OF THE ROAD", result.Songs[0].Title)
-			a.Equal("ROADHOUSE MEDLEY (ANNIVERSARY WALTZ PART 25)", result.Songs[74].Title)
+			a.Len(result.Songs, tt.expectLength)
+			a.Equal(tt.expectFirstSongTitle, result.Songs[0].Title)
+			a.Equal(tt.expectLastSongTitle, result.Songs[tt.expectLength - 1].Title)
 		})
 	}
 }
